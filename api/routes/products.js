bb58cbd75ee697d1f52e76363b5c6faafb14a770
Adrian Lee <adrianlee0118@gmail.com>
2020-03-03 14:25:07 -0800

    Added middleware as a relay station for parsing  resource (URL) and then directing to correct .js file in routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res,next) => {
+router.get('/', (req,res,next) => {    //path is './' because middleware in apps.js has already parsed the /products in the URL--in order to get to this file, the URL must already have '/products' in it
     res.status(200).json({
         message: 'Handling GET request to /products'
     });
@@ -10,6 +10,32 @@ router.get('/', (req,res,next) => {
 router.post('/', (req,res,next) => {
     res.status(200).json({
         message: 'Handling POST request to /products'
+    });
+});
+
+router.get('/:productId', (req,res,next) => {
+    const id = req.params.productId;
+    if (id === 'special'){
+        res.status(200).json({
+            message: 'You discovered the special ID!',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+});
+
+router.patch('/:productId', (req,res,next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req,res,next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
     });
 });
 
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-const ProductRoutes = require('./api/routes/products');
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
 //middleware - every request is funneled through here like a relay station
-app.use('/products', ProductRoutes);
+app.use('/products', productRoutes);     //use middleware to parse the resource (URL), so we don't have to do it in the products.js file
+app.use('/orders',orderRoutes);
 
 module.exports = app;
