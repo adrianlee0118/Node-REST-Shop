b0983fda7d89979b6a28c8d8f81e6358071f8b7e
Adrian Lee <adrianlee0118@gmail.com>
2020-03-03 16:35:26 -0800

    Added body parser package to reconcile JSON-formatted data in request body, added middleware to allow access to API from other applications

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
-app.use(morgan('dev'));   //middleware- all requests will be logged in terminal
+app.use(morgan("dev")); //middleware- default logging of all operations in terminal
+app.use(bodyParser.urlencoded({ extended: false })); //middleware- parsing JSON request body, slotting in received data appopriately
+app.use(bodyParser.json());
 
-//Routes which should handle requests - middleware directs to appopriate .js files
-app.use('/products', productRoutes);
-app.use('/orders',orderRoutes);
-
-app.use((req,res,next) => {
-    const error = new Error('Not found');
-    error.status(404);
-    next(error);
+app.use((req, res, next) => {                        //Send access headers to resolve CORS errors
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS'){                    //options are searched first, when searched allow all operations
+      res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+      return res.status(200).json({});
+  }
+  next();    //other routes can take over (rest of this file)
 });
 
-app.use((error,req,res,next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+
+//Routes which should handle requests - middleware directs to appopriate .js files
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 module.exports = app;
