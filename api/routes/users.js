c74769e533c7efd38539b70779f81a5436177ab4
Adrian Lee <adrianlee0118@gmail.com>
2020-03-05 22:27:13 -0800

    added user login to users.js routes, including user authentication (check if they exist), password verification (correct or not) and JWT (web token) signing.

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken"); //enable token that server passes back to client upon successful authorization (login) to indicate authentication (reusable)
 
 const User = require("../models/user");
 
@@ -45,6 +46,56 @@ router.post("/signup", (req, res, next) => {
           }
         });
       }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  //See if we got a user
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      //check if a user was found
+      if (user.length < 1) {
+        //user will be an array of 1
+        return res.status(401).json({
+          //don't say 'doesn't exist' because cyber attackers can use the msg to figure out what emails do exist on the db
+          message: "Auth failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          //if comparison found incorrect password
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        if (result) {
+          //comparison found correct password
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            process.env.JWT_KEY, //use environment variable from nodemon.json
+            {
+              expiresIn: "1h"
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed"
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
